bbe545789da5977cb4cdfdeb089b25cc128b810a
yardenroee <yardenroee@gmail.com>
2019-09-02 11:47:12 -0400

    first commit - test db migration

---
+++ database/migrations/1567439081126_test_schema.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class TestSchema extends Schema {
+  up () {
+    this.create('tests', (table) => {
+      table.increments()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('tests')
+  }
+}
+
+module.exports = TestSchema
