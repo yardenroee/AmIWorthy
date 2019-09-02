5015309c363fd78cf7eacca3ab56853bcf546e09
yardenroee <yardenroee@gmail.com>
2019-09-02 15:08:46 -0400

    adding views

---
+++ app/Controllers/Http/DiamondController.js
@@ -0,0 +1,12 @@
+'use strict'
+const Diamond = use('App/Models/Diamond')
+
+class DiamondController {
+    async index({request, response}) {
+        let diamonds = await Diamond.all();
+        return response.json(diamonds)
+    }
+
+}
+
+module.exports = DiamondController
--- start/routes.js
+++ start/routes.js
@@ -15,5 +15,5 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
-
-Route.on('/').render('welcome')
+Route.get('/', 'DiamondController.index');
+// Route.on('/').render('diamond-calc')
